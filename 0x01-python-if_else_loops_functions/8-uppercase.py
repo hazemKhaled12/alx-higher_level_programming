858d0dc790b8a79f5522f31c139fc12da89c7cc0
hazem Khaled <hazem.khaled1293@gmail.com>
2023-09-05 17:30:00 +0200

    to upper case

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+
+def uppercase(s):
+    for char in s:
+        if ord(char) >= ord("a") and ord(char) <= ord("z"):
+            print("{:s}".format(chr(ord(char) - 32)), end="")
+        else:
+            print("{:s}".format(char), end="")
