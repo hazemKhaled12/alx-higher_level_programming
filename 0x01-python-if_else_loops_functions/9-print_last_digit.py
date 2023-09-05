60215ac5cc379a3499244ba887a551689a405149
hazem Khaled <hazem.khaled1293@gmail.com>
2023-09-06 00:05:10 +0200

    last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-def print_last_digit(number):
+
+def getLastDigit(number):
     if number >= 0:
         return number % 10
     else:
-        return number % -10    
+        return (number % -10) * -1
+
+
+def print_last_digit(number):
+    lastDigit = getLastDigit(number)
+    print("{:d}".format(lastDigit), end="")
+    return lastDigit
