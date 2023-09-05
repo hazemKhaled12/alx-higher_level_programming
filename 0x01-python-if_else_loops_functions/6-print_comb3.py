85e66aea9e81a55fed7d6b66942d0b94ba34f0c9
hazem Khaled <hazem.khaled1293@gmail.com>
2023-09-05 17:04:34 +0200

    print non similar

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+for i in range(0, 10):
+    for j in range(1, 10):
+        if i == j or j < i:
+            continue
+
+        print("{:d}{:d}".format(i, j), end="\n" if i == 8 and j == 9 else ", ")
