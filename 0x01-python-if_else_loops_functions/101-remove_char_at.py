1a05223a4241d303a19790cc92b6a8cfe76c41da
hazem Khaled <hazem.khaled1293@gmail.com>
2023-09-06 00:52:02 +0200

    remove char

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+
+def remove_char_at(str, n):
+    newString = ''
+    for index, char in enumerate(str):
+        if index != n:
+            newString += char
+
+    return newString
