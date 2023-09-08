8877ba8bedd0d5319d4df7395c83a50e8cb37b6e
hazem Khaled <hazem.khaled1293@gmail.com>
2023-09-08 16:30:44 +0200

    replace element in list

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    length = len(my_list)
+
+    if idx < 0 or idx > length - 1:
+        return my_list
+
+    new_list = my_list[:]
+    new_list[idx] = element
+
+    return new_list
