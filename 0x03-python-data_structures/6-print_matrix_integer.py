e5993fb127b69a852e53e18abe7c1bed404a0ae7
hazem Khaled <hazem.khaled1293@gmail.com>
2023-09-09 17:43:58 +0200

    fixing task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return None
     for row in matrix:
         for num in row:
             print("{:d}".format(num), end=" ")
