62276bb836b3f99faa537954f2f2cdfc987d1c0d
hazem Khaled <hazem.khaled1293@gmail.com>
2023-10-09 23:34:10 +0300

    fix task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -15,8 +15,3 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-
-s = Square(13)
-
-print(s)
-print(s.area())
